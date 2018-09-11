76c7fbfe52228b8027857021e8628021509d4cfe
Gustavo Raña <gustavorana25@gmail.com>
2018-09-11 09:24:28 -0300

    theming button

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Button>Teste</Button>
+        <Button theme={{ mode: "primary" }}>Teste</Button>
       </div>
     );
   }
--- src/ui/Buttons/Buttons.js
+++ src/ui/Buttons/Buttons.js
@@ -1,9 +1,18 @@
 import styled from 'styled-components'
+import theme from 'styled-theming'
+
+const ButtonColors = theme('mode', {
+    default: '#f00',
+    primary: '#2185d0'
+})
+
+
 export const Button = styled.button`
-  display: inline-block;
-  border-radius: 3px;
-  padding: 0.5rem 0;
-  margin: 0.5rem 1rem;
-  width: 11rem;
-  border: 2px solid white;
+    display: inline-block;
+    border-radius: 3px;
+    padding: 0.5rem 0;
+    margin: 0.5rem 1rem;
+    width: 11rem;
+    border: 2px solid white;
+    background-color: ${ButtonColors};
 `
