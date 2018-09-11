094059d8cc7346e4073e9c26b070a9e3a24879a4
Gustavo Raña <gustavorana25@gmail.com>
2018-09-11 08:59:51 -0300

    frist component

---
+++ src/ui/Buttons/Buttons.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+export const Button = styled.button`
+  display: inline-block;
+  border-radius: 3px;
+  padding: 0.5rem 0;
+  margin: 0.5rem 1rem;
+  width: 11rem;
+  border: 2px solid white;
+`
